dfa71f6a6337efbfd38718b82e7802b393ee5d6d
giinandre <giinandre@gmail.com>
2026-05-02 17:38:50 -0500

    Add root redirect

--- config/urls.py
+++ config/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 from registro.views import estadisticas_admin
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/stage-lab/', permanent=False)),
     path('admin/estadisticas/', estadisticas_admin, name='admin_estadisticas'),
     path('admin/', admin.site.urls),
     path('', include('registro.urls')),
